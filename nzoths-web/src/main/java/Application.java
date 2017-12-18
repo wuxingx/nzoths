47eaf16cf6273310b4680a0325dfa587212b4a4c
Your Name <you@example.com>
2017-12-18 17:55:45 +0800

    1change

--- nzoths-web/src/main/java/Application.java
+++ nzoths-web/src/main/java/Application.java
@@ -3,6 +3,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
 import java.util.Date;
 
 @RestController
@@ -16,6 +17,53 @@ public class Application {
     @RequestMapping("/now")
     String hehe() {
         return "现在时间：" + (new Date()).toLocaleString();
+    }
+
+    @RequestMapping("/jdbc")
+    String jdbc() {
+        this.getAll();
+        return "1";
+    }
+    private static Connection getConn() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://www.wuxingx.top:3306/sys";
+        String username = "root";
+        String password = "";
+        Connection conn = null;
+        try {
+            Class.forName(driver); //classLoader,加载对应驱动
+            conn = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    private static Integer getAll() {
+        Connection conn = getConn();
+        String sql = "select * from sys_config";
+        PreparedStatement pstmt;
+        try {
+            pstmt = (PreparedStatement)conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            int col = rs.getMetaData().getColumnCount();
+            System.out.println("============================");
+            while (rs.next()) {
+                for (int i = 1; i <= col; i++) {
+                    System.out.print(rs.getString(i) + "\t");
+                    if ((i == 2) && (rs.getString(i).length() < 8)) {
+                        System.out.print("\t");
+                    }
+                }
+                System.out.println("");
+            }
+            System.out.println("============================");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void main(String[] args) {
