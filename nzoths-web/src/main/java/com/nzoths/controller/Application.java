baddf3c55535ed12cd14fb5384172946d961577f
wuxing <377469484@qq.com>
2018-03-03 23:49:48 +0800

    1.0

--- nzoths-web/src/main/java/com/nzoths/controller/Application.java
+++ nzoths-web/src/main/java/com/nzoths/controller/Application.java
@@ -1,11 +1,13 @@
 package com.nzoths.controller;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@MapperScan("com.nzoths.persist.mapping")
 public class Application {
 
     public static void main(String[] args) {
